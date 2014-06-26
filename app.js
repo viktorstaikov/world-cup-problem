15c89d6ec061c6d9f810942e7603da2c60d1ceed
Viktor Staikov <viktor.staikov@gmail.com>
2014-06-26 20:01:43 +0300

    status handler

--- app.js
+++ app.js
@@ -7,10 +7,48 @@ $(document).ready(function() {
       alert(textStatus);
     }
 
+    registerHelpers();
     listMatches(matches);
 
   });
 });
+
+function registerHelpers() {
+  Handlebars.registerHelper('status', function() {
+    var match = this;
+    var html = '<div>PUT SOME HTML</div>';
+
+    var startDate = new Date(match.datetime);
+    var hours = startDate.getHours();
+    var minutes = startDate.getMinutes();
+
+    if (match.status == 'future') {
+      var startAt = hours + ':' + (minutes < 10 ? '0' : '') + minutes + ' h';
+
+      html = '<div>Starts at: ' + startAt + '</div>';
+    } else {
+      var source = $('#progress-bar-tmpl').html();
+      var template = Handlebars.compile(source);
+
+      var now = new Date(Date.now());
+
+      var minutesPassed = (now.getHours() - hours) * 60 + now.getMinutes() - minutes;
+
+      if (45 < minutesPassed < 60) {
+        minutesPassed = 45;
+      }
+      if (60 < minutesPassed) {
+        minutesPassed -= 15;
+      }
+
+      html = template({
+        pct: minutesPassed * 100 / 90,
+        minLeft: 90 - minutesPassed
+      });
+    }
+    return html;
+  });
+}
 
 function listMatches(matches) {
   var source = $('#list-template').html();
@@ -19,5 +57,6 @@ function listMatches(matches) {
     matches: matches
   });
 
+  $('#main').empty();
   $('#main').append(html);
 }
