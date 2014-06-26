d2bf17372adbe428dfc09f7e9de14ef4c428ad24
staikov <staikov@staikov.(none)>
2014-06-26 19:21:04 +0300

    add flags and load-image styles; started basic templating

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 'use strict';
+
+$(document).ready(function() {
+
+  $.getJSON('http://worldcup.sfg.io/matches/today', function(matches, textStatus) {
+    if (textStatus != 'success') {
+      alert(textStatus);
+    }
+
+    listMatches(matches);
+
+  });
+});
+
+function listMatches(matches) {
+  var source = $('#list-template').html();
+  var template = Handlebars.compile(source);
+  var html = template({
+    matches: matches
+  });
+
+  $('#main').append(html);
+}
